aabf15745587c0a8c6fa199debc50c9bb28d420b
Ben Tinworth <ben.tinworth@gmail.com>
2016-02-28 18:09:11 +1100

    IE Fixes + Favicon

--- wekan-view.js
+++ wekan-view.js
@@ -4,13 +4,14 @@ if (Meteor.isClient) {
   Meteor.subscribe('cards');
 
   Session.setDefault('dates', '');
-  Template.summary.helpers({
+  Template.datesummary.helpers({
     commits: function (date) {
       var ids = [];
       var cards = Cards.find({ $and: [
-        { 'commits': { $exists: true }},
-        {'dueAt': { $exists: true }}
+        { 'commits':  { $exists: true }},
+        { 'dueAt':    { $exists: true }}
       ]});
+
       cards.forEach(function (card) {
         if (card.hasOwnProperty('dueAt') && date == moment(card.dueAt).format('dddd (LL)')) {
           for (var i = 0; i < card.commits.length; ++i) {
@@ -18,23 +19,28 @@ if (Meteor.isClient) {
           }
         }
       });
+
       return ids;
     },
+
     dates: function () {
       var dates = [];
       var cards = Cards.find({ $and: [
-        { 'commits': { $exists: true }},
-        {'dueAt': { $exists: true }}
+        { 'commits':  { $exists: true }},
+        { 'dueAt':    { $exists: true }}
       ]});
+
       cards.forEach(function (card) {
         if (card.hasOwnProperty('dueAt')) {
           dates.push(moment(card.dueAt).format('YYYY MM DD'));
         }
       });
       dates.sort();
+
       for (var i = 0; i < dates.length; ++i) {
-        dates[i] = moment(dates[i]).format('dddd (LL)');
+        dates[i] = moment(dates[i], 'YYYY MM DD').format('dddd (LL)');
       }
+
       return dates;
     }
   });
