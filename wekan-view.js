2066308dc409114104bb85cacec46fd41d46be2c
Ben Tinworth <ben.tinworth@gmail.com>
2016-02-28 17:24:05 +1100

    Adding Date and Commit summary

--- wekan-view.js
+++ wekan-view.js
@@ -1,17 +1,41 @@
+Cards = new Mongo.Collection('cards');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Meteor.subscribe('cards');
 
+  Session.setDefault('dates', '');
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    commits: function (date) {
+      var ids = [];
+      var cards = Cards.find({ $and: [
+        { 'commits': { $exists: true }},
+        {'dueAt': { $exists: true }}
+      ]});
+      cards.forEach(function (card) {
+        if (card.hasOwnProperty('dueAt') && date == moment(card.dueAt).format('dddd (LL)')) {
+          for (var i = 0; i < card.commits.length; ++i) {
+            ids.push(card.commits[i]);
+          }
+        }
+      });
+      return ids;
+    },
+    dates: function () {
+      var dates = [];
+      var cards = Cards.find({ $and: [
+        { 'commits': { $exists: true }},
+        {'dueAt': { $exists: true }}
+      ]});
+      cards.forEach(function (card) {
+        if (card.hasOwnProperty('dueAt')) {
+          dates.push(moment(card.dueAt).format('YYYY MM DD'));
+        }
+      });
+      dates.sort();
+      for (var i = 0; i < dates.length; ++i) {
+        dates[i] = moment(dates[i]).format('dddd (LL)');
+      }
+      return dates;
     }
   });
 }
